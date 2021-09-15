9fec039f3b2b0ec730ceece930ac61b639e6bb85
Кирпинёв Игорь Валерьевич <ikirpinev@beeline.ru>
2021-09-15 23:13:10 +0300

    fix: update solution for hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,26 +14,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-// export default function calculateHanoi(disks, turnsSpeed) {
-//   const turns = Math.pow(2, disks) - 1;
-//   let seconds = (turns / (turnsSpeed / 60)) * 60;
-//
-//   if (String(seconds).split(".")[1].match(/9/g) && String(seconds).split(".")[1].match(/9/g).length
-//       === String(seconds).split(".")[1].length) {
-//     seconds = Math.ceil(seconds)
-//   } else {
-//     seconds = Math.floor(seconds);
-//   }
-//
-//   return {
-//     turns,
-//     seconds,
-//   }
-// }
-
 export default function calculateHanoi(disks, turnsSpeed) {
   const turns = Math.pow(2, disks) - 1;
-  const seconds = Math.floor((turns / (turnsSpeed / 60)) * 60);
+  let seconds = (turns / (turnsSpeed / 60)) * 60;
+
+  if (String(seconds).split(".")[1].match(/9/g)
+      && String(seconds).split(".")[1].match(/9/g).length
+      === String(seconds).split(".")[1].length) {
+    seconds = Math.ceil(seconds)
+  } else {
+    seconds = Math.floor(seconds);
+  }
 
   return {
     turns,
